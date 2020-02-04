c3448969b3da6d47e29b814895040724b3013105
davidMolinaBastos <dmolinab@edu.tecnocampus.cat>
2020-02-04 15:47:56 +0100

    Añadidos PlayerController y NodoController

---
+++ Piritis/Assets/Scripts/NodoScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodoScript : MonoBehaviour
+{
+    // 0 = Norte 
+    //1 = Este 
+    //2 = Sur
+    // 3 = Oeste
+    public Dictionary<string, GameObject> CloseNodes;
+
+    public List<GameObject> nodos;
+
+    public void Start()
+    {
+
+    }
+
+    public Dictionary<string, GameObject> getDicionary()
+    {
+        //
+        if (nodos[0] != null)
+        {
+            Debug.Log("sdfdsf");
+            CloseNodes.Add("Norte", nodos[0]);
+        }
+        else
+            CloseNodes.Add("Norte", null);
+        //
+        if (nodos[1] != null)
+            CloseNodes.Add("Este", nodos[1]);
+        else
+            CloseNodes.Add("Este", null);
+        //
+        if (nodos[2] != null)
+            CloseNodes.Add("Sur", nodos[2]);
+        else
+            CloseNodes.Add("Sur", null);
+        //
+        if (nodos[3] != null)
+            CloseNodes.Add("Oeste", nodos[3]);
+        else
+            CloseNodes.Add("Oeste", null);
+
+        return CloseNodes;
+    }
+
+}
---
+++ Piritis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public KeyCode m_LeftKeyCode = KeyCode.A;
+    public KeyCode m_RightKeyCode = KeyCode.D;
+    public KeyCode m_UpKeyCode = KeyCode.W;
+    public KeyCode m_DownKeyCode = KeyCode.S;
+
+    private NodoScript ActualNode;
+    private Dictionary<string, GameObject> PossiblePositions;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Movimiento
+        if (Input.GetKey(m_UpKeyCode) && PossiblePositions["Norte"] == true)
+        {
+            this.transform.position = PossiblePositions["Nord"].transform.position;
+        }
+        else if (Input.GetKey(m_DownKeyCode) && PossiblePositions["Sur"] == true)
+        {
+            this.transform.position = PossiblePositions["Sur"].transform.position;
+        }
+        else if (Input.GetKey(m_RightKeyCode) && PossiblePositions["Este"] == true)
+        {
+            this.transform.position = PossiblePositions["Este"].transform.position;
+        }
+        else if (Input.GetKey(m_LeftKeyCode) && PossiblePositions["Oeste"] == true)
+        {
+            this.transform.position = PossiblePositions["Oeste"].transform.position;
+        }
+
+
+    }
+
+    void OnTriggerEnter2D(Collider2D _Collider)
+    {
+        Debug.Log("ontrigger");
+
+        if (_Collider.tag == "Nodo")
+        {
+            Debug.Log("Collider");
+            ActualNode = _Collider.GetComponent<NodoScript>();
+            PossiblePositions = ActualNode.getDicionary();
+
+        }
+    }
+}
